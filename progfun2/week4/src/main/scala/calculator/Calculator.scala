cbe57c2be8f4ad611d069fc0caaa6414d3076065
Juan Roca <juancroca@gmail.com>
2017-04-12 22:32:10 +0200

    week4

--- progfun2/week4/src/main/scala/calculator/Calculator.scala
+++ progfun2/week4/src/main/scala/calculator/Calculator.scala
@@ -9,20 +9,35 @@ final case class Times(a: Expr, b: Expr) extends Expr
 final case class Divide(a: Expr, b: Expr) extends Expr
 
 object Calculator {
-  def computeValues(
-      namedExpressions: Map[String, Signal[Expr]]): Map[String, Signal[Double]] = {
-    ???
+  val path = Var(List[String]())
+  def computeValues(namedExpressions: Map[String, Signal[Expr]]): Map[String, Signal[Double]] = {
+    namedExpressions.mapValues{ exp =>
+      Signal(eval(exp(), namedExpressions))
+    }
   }
 
   def eval(expr: Expr, references: Map[String, Signal[Expr]]): Double = {
-    ???
+    def binop(op : (Double, Double) => Double)(a: Expr, b: Expr) = op(eval(a, references), eval(b, references))
+    expr match{
+      case Literal(v) if path().contains(v) => Double.NaN
+      case Literal(v) => v
+      case Ref(name) => {
+        val p = path()
+        path.update(p.::(name))
+        eval(getReferenceExpr(name, references), references)
+      }
+      case Plus(a, b) => binop(_ + _)(a, b)
+      case Minus(a, b) => binop(_ - _)(a, b)
+      case Times(a, b) => binop(_ * _)(a, b)
+      case Divide(a, b) => binop(_ / _)(a, b)
+    }
   }
+
 
   /** Get the Expr for a referenced variables.
    *  If the variable is not known, returns a literal NaN.
    */
-  private def getReferenceExpr(name: String,
-      references: Map[String, Signal[Expr]]) = {
+  private def getReferenceExpr(name: String, references: Map[String, Signal[Expr]]) = {
     references.get(name).fold[Expr] {
       Literal(Double.NaN)
     } { exprSignal =>
--- progfun2/week4/src/main/scala/calculator/Polynomial.scala
+++ progfun2/week4/src/main/scala/calculator/Polynomial.scala
@@ -3,11 +3,21 @@ package calculator
 object Polynomial {
   def computeDelta(a: Signal[Double], b: Signal[Double],
       c: Signal[Double]): Signal[Double] = {
-    ???
+    (a(), b(), c()) match {
+      case(ai, bi, ci) => Signal(bi * bi - (4 * ai * ci))
+    }
   }
 
   def computeSolutions(a: Signal[Double], b: Signal[Double],
       c: Signal[Double], delta: Signal[Double]): Signal[Set[Double]] = {
-    ???
+    (a()*2, b(), delta()) match {
+      case(ai, bi, di) => Signal({
+        Math.sqrt(di) match {
+          case dsqrt if dsqrt.isNaN => Set()
+          case dsqrt if dsqrt == 0 => Set(-bi / ai)
+          case dsqrt => Set((-bi - dsqrt)/ai, (-bi + dsqrt)/ai)
+        }
+      })
+    }
   }
 }
